1d6d853bee7dd33227fa9cdbc60da6ce21e25221
curtilew <182793266+curtilew@users.noreply.github.com>
2025-03-27 15:10:03 -0400

    Add function getUserChoice to js file

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,4 +2,19 @@ function getComputerChoice() {
     return Math.random() + Math.random() + Math.random()
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    let userChoice = prompt("Pick Rock, Paper, or Scissors: ");
+    
+    if (userChoice.toLowerCase() == "rock") {
+        return 1
+    } else if (userChoice.toLowerCase() == "paper") {
+        return 1
+    } else if (userChoice.toLowerCase() == "scissors") {
+        return 1
+    }  else {
+        alert("Invalid choice");
+        getHumanChoice()
+    }
+}
+
+console.log(getHumanChoice())
